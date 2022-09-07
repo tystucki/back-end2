f27b5ab10f9b78f0adbcc7ee2818590913267f20
Ty Stucki <tystucki@Tys-MacBook-Pro.local>
2022-09-07 14:28:53 -0700

    First Init

---
+++ server/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+
+
+const controller = require('./controller');
+const {getHouses, deleteHouse, createHouse, updateHouse} = controller
+
+app.get('/api/houses', getHouses);
+app.post('/api/houses', createHouse);
+app.put('/api/houses/:id', updateHouse);
+app.delete('/api/houses/:id', deleteHouse);
+
+app.listen(4004, () => console.log('Listening to port 4004'))
